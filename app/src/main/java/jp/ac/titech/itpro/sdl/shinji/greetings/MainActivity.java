5176a5c5e48d978109422c95743dc938a2ad2187
sumoooru2 <sumoooru2>
2016-04-22 15:52:57 +0900

    assignment4_22

--- app/src/main/java/jp/ac/titech/itpro/sdl/shinji/greetings/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/shinji/greetings/MainActivity.java
@@ -1,17 +1,16 @@
 package jp.ac.titech.itpro.sdl.shinji.greetings;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity /*implements View.OnClickListener*/{
 
     private TextView outputView;
     private EditText inputName;
-    private Button okButton;
+    //private Button okButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,19 +19,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         outputView = (TextView)findViewById(R.id.output_view);
         inputName = (EditText)findViewById(R.id.input_name);
-        okButton = (Button)findViewById(R.id.ok_button);
-        okButton.setOnClickListener(this);
+        //okButton = (Button)findViewById(R.id.ok_button);
+        //okButton.setOnClickListener(this);
     }
 
-    @Override
-    public void onClick(View v){
-        switch(v.getId()){
-            case R.id.ok_button:
-                String name = inputName.getText().toString();
-                if(name.length() > 0){
-                    outputView.setText("Hello, " + name + "\nNice to see you!");
-                    break;
-                }
+//    @Override
+//    public void onClick(View v){
+//        switch(v.getId()){
+//            case R.id.ok_button:
+//                String name = inputName.getText().toString();
+//                if(name.length() > 0){
+//                    outputView.setText("Hello, " + name + "\nNice to see you!");
+//                    break;
+//                }
+//        }
+//    }
+
+    public void greet(View v){
+        String name = inputName.getText().toString();
+        if(name.length() > 0){
+            //outputView.setText("Hello, " + name + "\nNice to see you!");
+            outputView.setText(String.format("Hello, %s\nNice to see you!", name));
         }
     }
 }
